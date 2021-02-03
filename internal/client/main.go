6062e70e8afed23f66c597483dedbf5ad6b07919
bm@krishna <mk.bodimalla@gmail.com>
2021-02-04 00:47:44 +0530

    adding local and docker ip address

--- internal/client/main.go
+++ internal/client/main.go
@@ -12,9 +12,9 @@ func ConfigClient() (tenant.TenantClient, error) {
 	// client will send request to server by using grpc.Dail wich takes server address
 	address := os.Getenv("ADDRESS")
 	if address == "" {
-		address = "7171"
+		address = ":7575"
 	}
-	clientConnection, err := grpc.Dial("172.17.0.1:"+address, grpc.WithInsecure())
+	clientConnection, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		log.Fatal("Server Didn't connect", err)
 		return nil, err
